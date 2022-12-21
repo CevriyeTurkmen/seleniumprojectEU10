eca9b1b5983aa0ef0f7450162755afbdf19e9694
Cevriye Berkcan Turkmen <cberkcan@gmail.com>
2022-12-21 23:45:16 +0000

    day1 Task 1

---
+++ src/test/java/com/cydeo/tests/day1_selenium_intro/Task1_YahooTitleVerification.java
@@ -0,0 +1,17 @@
+package com.cydeo.tests.day1_selenium_intro;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Task1_YahooTitleVerification {
+    public static void main(String[] args) {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver=new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://www.yahoo.com");
+        System.out.println("driver.getTitle() = " + driver.getTitle());
+
+
+    }
+}
