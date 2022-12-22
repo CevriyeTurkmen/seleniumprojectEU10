85b90e6b400988db28c12b1af0a09fe0c4f0b5d2
Cevriye Berkcan Turkmen <cberkcan@gmail.com>
2022-12-22 20:07:58 +0000

    day2 Task 1

--- src/test/java/com/cydeo/tests/day2_locators_getText_getAttribute/T1_CydeoVerifications.java
+++ src/test/java/com/cydeo/tests/day2_locators_getText_getAttribute/T1_CydeoVerifications.java
@@ -8,16 +8,25 @@ public class T1_CydeoVerifications {
     public static void main(String[] args) {
 
         WebDriverManager.chromedriver().setup();
-        WebDriver driver=new ChromeDriver();
+        WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get("https://practice.cydeo.com");
-        String expectedURL="cydeo";
-        String actualURL= driver.getCurrentUrl();
-        if (actualURL.contains(expectedURL)){
-            System.out.println("expectedURL = " + expectedURL);;
+        String expectedURL = "cydeo";
+        String actualURL = driver.getCurrentUrl();
+        if (actualURL.contains(expectedURL)) {
+            System.out.println("Passed");
+        } else {
+            System.out.println("Failed");
         }
-        System.out.println("driver.getTitle() = " + driver.getTitle());
+            String expectedTitle = "Practice";
+            String actualTitle = driver.getTitle();
+            if (expectedTitle.equals(actualTitle)) {
+                System.out.println("Title Passed");
+            } else {
+                System.out.println("Title Failed");
 
 
+            }
+        }
     }
-}
+
