031699242c86e13fada7bd83996258609259aaec
Cevriye Berkcan Turkmen <cberkcan@gmail.com>
2022-12-22 00:10:03 +0000

    day1 Task 1

--- src/test/java/com/cydeo/tests/day1_selenium_intro/Task1_YahooTitleVerification.java
+++ src/test/java/com/cydeo/tests/day1_selenium_intro/Task1_YahooTitleVerification.java
@@ -10,8 +10,14 @@ public class Task1_YahooTitleVerification {
         WebDriver driver=new ChromeDriver();
         driver.manage().window().maximize();
         driver.get("https://www.yahoo.com");
-        System.out.println("driver.getTitle() = " + driver.getTitle());
-
+        String expectedTitle="Yahoo UK | News, email and search";
+        String actualTitle= driver.getTitle();
+        if (actualTitle.equals(expectedTitle)){
+            System.out.println("Passed");
+        }else {
+            System.out.println("Failed");
+        }
+        driver.quit();
 
     }
 }
