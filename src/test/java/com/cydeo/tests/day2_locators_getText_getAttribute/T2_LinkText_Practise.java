81f14f59c288193f7396b6d513bd80576c960fad
Cevriye Berkcan Turkmen <cberkcan@gmail.com>
2022-12-22 20:01:38 +0000

    day2 Task 2

---
+++ src/test/java/com/cydeo/tests/day2_locators_getText_getAttribute/T2_LinkText_Practise.java
@@ -0,0 +1,20 @@
+package com.cydeo.tests.day2_locators_getText_getAttribute;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class T2_LinkText_Practise {
+    public static void main(String[] args) {
+
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver=new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://practice.cydeo.com");
+WebElement abTest= driver.findElement(By.linkText("A/B Testing"));
+abTest.click();
+
+    }
+}
