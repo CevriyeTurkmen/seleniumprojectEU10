7705181bccfaa37ddfb5fdb2269e49c6176b8cb8
Cevriye Berkcan Turkmen <cberkcan@gmail.com>
2022-12-22 22:22:51 +0000

    day2 Task 3

--- src/test/java/com/cydeo/tests/day2_locators_getText_getAttribute/T3_GoogleSearch.java
+++ src/test/java/com/cydeo/tests/day2_locators_getText_getAttribute/T3_GoogleSearch.java
@@ -1,7 +1,10 @@
 package com.cydeo.tests.day2_locators_getText_getAttribute;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class T3_GoogleSearch {
@@ -10,6 +13,20 @@ public class T3_GoogleSearch {
         WebDriver driver=new ChromeDriver();
         driver.manage().window().maximize();
         driver.get("https://google.com");
+        WebElement agreeButton= driver.findElement(By.id("L2AGLb"));
+        agreeButton.click();
+        WebElement googleSearchBox=driver.findElement(By.name("q"));
+        googleSearchBox.sendKeys("apple"+ Keys.ENTER);
+        String ExpectedTitle="apple";
+        String ActualTitle= driver.getTitle();
+
+        if (ActualTitle.startsWith(ExpectedTitle)){
+            System.out.println("Passed");
+        }else {
+            System.out.println("Failed");}
+
+
+
 
 
 
