cde8910742cfa133d7a225dea12b6b52570edbcd
Cevriye Berkcan Turkmen <cberkcan@gmail.com>
2022-12-22 21:48:13 +0000

    day2 Task 3

--- src/test/java/com/cydeo/tests/day2_locators_getText_getAttribute/T3_GoogleSearch.java
+++ src/test/java/com/cydeo/tests/day2_locators_getText_getAttribute/T3_GoogleSearch.java
@@ -1,7 +1,30 @@
 package com.cydeo.tests.day2_locators_getText_getAttribute;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class T3_GoogleSearch {
     public static void main(String[] args) {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver=new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://google.com");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     }
 }
