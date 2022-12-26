25d74ccce7e3a56c39523ce33694b3084d45bf6c
Cevriye Berkcan Turkmen <cberkcan@gmail.com>
2022-12-26 21:41:20 +0000

    day3 Task1

---
+++ src/test/java/com/cydeo/tests/day3_cssSelector_xpath/T1_locators_getText.java
@@ -0,0 +1,26 @@
+package com.cydeo.tests.day3_cssSelector_xpath;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class T1_locators_getText {
+    public static void main(String[] args) {
+
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver=new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://login1.nextbasecrm.com/  ");
+      //  driver.findElement(By.name("USER_LOGIN")).sendKeys("Incorrect");
+        WebElement inputUserName=driver.findElement(By.name("USER_LOGIN"));
+        inputUserName.sendKeys("Incorrect");
+
+
+
+
+
+
+    }
+}
