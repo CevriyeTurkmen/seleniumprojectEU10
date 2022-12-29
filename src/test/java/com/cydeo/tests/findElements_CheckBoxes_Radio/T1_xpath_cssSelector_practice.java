f035ca5129cc2b4f767941b235ee73df9f9f5522
Cevriye Berkcan Turkmen <cberkcan@gmail.com>
2022-12-29 22:11:47 +0000

    day4 Task 1

--- src/test/java/com/cydeo/tests/findElements_CheckBoxes_Radio/T1_xpath_cssSelector_practice.java
+++ src/test/java/com/cydeo/tests/findElements_CheckBoxes_Radio/T1_xpath_cssSelector_practice.java
@@ -1,7 +1,19 @@
 package com.cydeo.tests.findElements_CheckBoxes_Radio;
 
+import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
 public class T1_xpath_cssSelector_practice {
     public static void main(String[] args) {
+
+        WebDriver driver= WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+        driver.get("https://practice.cydeo.com/forgot_password ");
+        WebElement homeLink1=driver.findElement(By.cssSelector("a[class='nav-link']"));
+        WebElement homeLink2=driver.findElement(By.cssSelector("a.nav-link"));
+
 
 
 
