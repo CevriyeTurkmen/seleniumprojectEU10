e06988da169f71c3ebddd70625dbaa61e26c3205
Cevriye Berkcan Turkmen <cberkcan@gmail.com>
2022-12-30 15:43:24 +0000

    day4 Task 4

--- src/test/java/com/cydeo/tests/findElements_CheckBoxes_Radio/T4_findElements.java
+++ src/test/java/com/cydeo/tests/findElements_CheckBoxes_Radio/T4_findElements.java
@@ -1,8 +1,11 @@
 package com.cydeo.tests.findElements_CheckBoxes_Radio;
 
 import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class T4_findElements {
@@ -12,6 +15,11 @@ public class T4_findElements {
         driver.manage().window().maximize();
         driver.get("https://practice.cydeo.com/abtest ");
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        List<WebElement> allLinks = driver.findElements(By.tagName("a"));
+        System.out.println("allLinks.size() = " + allLinks.size());
+        for (WebElement each : allLinks) {
+            System.out.println("Test of links "+ each.getText());
+        }
 
 
     }
