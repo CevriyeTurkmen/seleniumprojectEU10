fe58f4811d8aa23fa03a5875acfc3e4272d5d140
Cevriye Berkcan Turkmen <cberkcan@gmail.com>
2023-01-06 20:16:57 +0000

    day5 task5

--- src/test/java/com/cydeo/tests/day6_alerts_iframe_Windows/DropdownPractices.java
+++ src/test/java/com/cydeo/tests/day6_alerts_iframe_Windows/DropdownPractices.java
@@ -1,21 +1,34 @@
 package com.cydeo.tests.day6_alerts_iframe_Windows;
 
 import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.util.concurrent.TimeUnit;
 
 public class DropdownPractices {
     WebDriver driver;
-    @Test
-    public void dropDown_task5(){
 
-        driver= WebDriverFactory.getDriver("chrome");
+    @Test
+    public void dropDown_task5() {
+
+        driver = WebDriverFactory.getDriver("chrome");
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.get("http://practice.cybertekschool.com/dropdown ");
 
+        Select dropdown = new Select(driver.findElement(By.xpath("//select[@id='state']")));
+        dropdown.selectByVisibleText("Illinois");
+        dropdown.selectByValue("VA");
+        dropdown.selectByIndex(5);
+
+        String expected="California";
+        String actual=dropdown.getFirstSelectedOption().getText();
+
+        Assert.assertEquals(expected,actual);
 
     }
 
