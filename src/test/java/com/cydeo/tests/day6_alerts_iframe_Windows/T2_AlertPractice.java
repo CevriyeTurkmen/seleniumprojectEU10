6f2ff41d3fef481f46246d42abd87ae026f48ecf
Cevriye Berkcan Turkmen <cberkcan@gmail.com>
2023-01-08 16:37:09 +0000

    day6 task2

--- src/test/java/com/cydeo/tests/day6_alerts_iframe_Windows/T2_AlertPractice.java
+++ src/test/java/com/cydeo/tests/day6_alerts_iframe_Windows/T2_AlertPractice.java
@@ -1,7 +1,11 @@
 package com.cydeo.tests.day6_alerts_iframe_Windows;
 
 import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -19,6 +23,17 @@ public class T2_AlertPractice {
     }
     @Test
     public void AlertTask2(){
+
+        WebElement JsConfirm= driver.findElement(By.xpath("//button[.='Click for JS Confirm']"));
+        JsConfirm.click();
+        Alert alert=driver.switchTo().alert();
+        alert.accept();
+
+        WebElement Result=driver.findElement(By.xpath("//p[@style='color:green']"));
+        String expected="You clicked: Ok";
+        String actual= Result.getText();
+        Assert.assertEquals(expected,actual);
+
 
     }
 
