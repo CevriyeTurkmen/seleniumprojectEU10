45ff46cfb3b70c74a6a2f3eafaafbb7dcc8f4bff
Cevriye Berkcan Turkmen <cberkcan@gmail.com>
2023-01-09 20:32:05 +0000

    utilities

--- src/test/java/com/cydeo/utilities/BrowserUtils.java
+++ src/test/java/com/cydeo/utilities/BrowserUtils.java
@@ -1,5 +1,10 @@
 package com.cydeo.utilities;
 
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+
+import java.util.Set;
+
 public class BrowserUtils {
 
     public static void sleep(int second) {
@@ -11,4 +16,49 @@ public class BrowserUtils {
         }
 
     }
+
+    public static void switchWindowAndVerify(WebDriver driver, String expectedInURL, String expectedinTitle ){
+
+
+        Set<String> allWindowsHandles = driver.getWindowHandles();
+
+        for (String each : allWindowsHandles) {
+            driver.switchTo().window(each);
+
+            System.out.println("Current URL= " + driver.getCurrentUrl());
+
+            if (driver.getCurrentUrl().contains(expectedInURL)) {
+                break;
+            }
+        }
+
+        String actualTitle= driver.getTitle();
+
+        Assert.assertTrue(actualTitle.contains(expectedinTitle));
+    }
+
+public static void verifyTitle(WebDriver driver,String expectedTitle){
+        Assert.assertEquals(driver.getTitle(),expectedTitle);
 }
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
