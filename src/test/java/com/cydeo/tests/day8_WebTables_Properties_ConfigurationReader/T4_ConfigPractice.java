c091e9104de6406fb8542d4c338bcb055bd4de11
Cevriye Berkcan Turkmen <cberkcan@gmail.com>
2023-01-11 15:04:16 +0000

    Google Search Test

---
+++ src/test/java/com/cydeo/tests/day8_WebTables_Properties_ConfigurationReader/T4_ConfigPractice.java
@@ -0,0 +1,47 @@
+package com.cydeo.tests.day8_WebTables_Properties_ConfigurationReader;
+
+import com.cydeo.utilities.ConfigurationReader;
+import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class T4_ConfigPractice {
+
+    public WebDriver driver;
+
+    @BeforeMethod
+
+    public void SetUpMethod() {
+
+        String browserType= ConfigurationReader.getProperty("browser");
+
+        driver = WebDriverFactory.getDriver(browserType);
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get("https://google.com ");
+    }
+
+    @Test
+    public void google_search_test(){
+
+        WebElement googleSearchBox= driver.findElement(By.xpath("//input[@name='q']"));
+        WebElement acceptbutton= driver.findElement(By.xpath("//div[.='Accept all']"));
+        acceptbutton.click();
+        googleSearchBox.sendKeys("apple"+ Keys.ENTER);
+
+        String expectedTitle="apple - Google Search";
+        String actualTitle= driver.getTitle();
+
+        Assert.assertEquals(actualTitle,expectedTitle);
+
+
+
+    }
+}
