ee356645ed74454851b53da174d98fc0b3e4bfaa
Cevriye Berkcan Turkmen <cberkcan@gmail.com>
2023-01-11 11:34:19 +0000

    day 8 properties

---
+++ src/test/java/com/cydeo/tests/day8_WebTables_Properties_ConfigurationReader/ReadingProperties.java
@@ -0,0 +1,21 @@
+package com.cydeo.tests.day8_WebTables_Properties_ConfigurationReader;
+
+import org.testng.annotations.Test;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class ReadingProperties {
+
+    @Test
+    public void reading_from_properties_test() throws IOException {
+        Properties properties= new Properties();
+        FileInputStream file=new FileInputStream("configuration.properties");
+        properties.load(file);
+        System.out.println("properties.getProperty(\"browser\") = " + properties.getProperty("browser"));
+        System.out.println("properties.getProperty(\"env\") = " + properties.getProperty("env"));
+        System.out.println("properties.getProperty(\"username\") = " + properties.getProperty("username"));
+
+    }
+}
