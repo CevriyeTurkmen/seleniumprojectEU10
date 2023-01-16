fcc8b8c576e10dbd86365c8a5909bac46b5e93da
Cevriye Berkcan Turkmen <cberkcan@gmail.com>
2023-01-16 21:20:04 +0000

    Test Base

--- src/test/java/com/cydeo/tests/base/TestBase.java
+++ src/test/java/com/cydeo/tests/base/TestBase.java
@@ -2,6 +2,7 @@ package com.cydeo.tests.base;
 
 import com.cydeo.utilities.WebDriverFactory;
 import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
 import java.util.concurrent.TimeUnit;
@@ -18,6 +19,12 @@ public abstract class TestBase {
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
+    }
+
+    @AfterMethod
+
+    public void tearDown(){
+        driver.close();
     }
 
 }
