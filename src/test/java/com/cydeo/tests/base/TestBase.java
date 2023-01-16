5f0426a31b25633f4ee35baa68424530de1c53e5
Cevriye Berkcan Turkmen <cberkcan@gmail.com>
2023-01-16 18:37:41 +0000

    TestBase.java

---
+++ src/test/java/com/cydeo/tests/base/TestBase.java
@@ -0,0 +1,23 @@
+package com.cydeo.tests.base;
+
+import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.BeforeMethod;
+
+import java.util.concurrent.TimeUnit;
+
+public abstract class TestBase {
+
+   public WebDriver driver;
+
+    @BeforeMethod
+
+    public void SetUpMethod() {
+
+        driver = WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+    }
+
+}
