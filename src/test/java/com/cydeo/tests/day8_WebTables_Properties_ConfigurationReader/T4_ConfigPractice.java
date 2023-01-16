a6a9e0d2f9ebfb11ccd85800f6bcd12e10b222d0
Cevriye Berkcan Turkmen <cberkcan@gmail.com>
2023-01-16 21:19:45 +0000

    Day 8

--- src/test/java/com/cydeo/tests/day8_WebTables_Properties_ConfigurationReader/T4_ConfigPractice.java
+++ src/test/java/com/cydeo/tests/day8_WebTables_Properties_ConfigurationReader/T4_ConfigPractice.java
@@ -1,43 +1,40 @@
 package com.cydeo.tests.day8_WebTables_Properties_ConfigurationReader;
 
 import com.cydeo.utilities.ConfigurationReader;
-import com.cydeo.utilities.WebDriverFactory;
+import com.cydeo.utilities.Driver;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-
-import java.util.concurrent.TimeUnit;
 
 public class T4_ConfigPractice {
 
-    public WebDriver driver;
+   // public WebDriver driver;
 
-    @BeforeMethod
+  //  @BeforeMethod
 
-    public void SetUpMethod() {
+   // public void SetUpMethod() {
 
-        String browserType= ConfigurationReader.getProperty("browser");
+     //   String browserType= ConfigurationReader.getProperty("browser");
 
-        driver = WebDriverFactory.getDriver(browserType);
-        driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        driver.get("https://google.com ");
-    }
+     //   driver = WebDriverFactory.getDriver(browserType);
+    //    driver.manage().window().maximize();
+   //     driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+  //  }
 
     @Test
     public void google_search_test(){
 
-        WebElement googleSearchBox= driver.findElement(By.xpath("//input[@name='q']"));
-        WebElement acceptbutton= driver.findElement(By.xpath("//div[.='Accept all']"));
+        Driver.getDriver().get("https://google.com ");
+        WebElement googleSearchBox= Driver.getDriver().findElement(By.xpath("//input[@name='q']"));
+        WebElement acceptbutton= Driver.getDriver().findElement(By.xpath("//div[.='Accept all']"));
         acceptbutton.click();
         googleSearchBox.sendKeys(ConfigurationReader.getProperty("searchValue")+ Keys.ENTER);
 
         String expectedTitle=ConfigurationReader.getProperty("searchValue") +" - Google Search";
-        String actualTitle= driver.getTitle();
+        String actualTitle= Driver.getDriver().getTitle();
 
         Assert.assertEquals(actualTitle,expectedTitle);
 
