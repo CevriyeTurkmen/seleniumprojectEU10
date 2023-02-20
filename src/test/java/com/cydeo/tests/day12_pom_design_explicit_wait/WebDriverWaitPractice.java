2be0c67cbe477a0b1c9481863b7d13968d1e42ce
Cevriye Berkcan Turkmen <cberkcan@gmail.com>
2023-02-20 11:46:55 +0000

    day_12

--- src/test/java/com/cydeo/tests/day12_pom_design_explicit_wait/WebDriverWaitPractice.java
+++ src/test/java/com/cydeo/tests/day12_pom_design_explicit_wait/WebDriverWaitPractice.java
@@ -1,5 +1,6 @@
 package com.cydeo.tests.day12_pom_design_explicit_wait;
 
+import com.cydeo.pages.DynamicLoad1Page;
 import com.cydeo.pages.DynamicLoad7Page;
 import com.cydeo.utilities.Driver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -19,6 +20,27 @@ public class WebDriverWaitPractice {
 
         Assert.assertTrue(dynamicLoad7Page.doneMessage.isDisplayed());
         Assert.assertTrue(dynamicLoad7Page.spongeBobImage.isDisplayed());
+
+
+
+
+    }
+
+    @Test
+    public void dynamic_load_1_test(){
+
+        Driver.getDriver().get("https://practice.cydeo.com/dynamic_loading/1  ");
+        DynamicLoad1Page dynamicLoad1Page=new DynamicLoad1Page();
+        dynamicLoad1Page.startButton.click();
+        WebDriverWait wait=new WebDriverWait(Driver.getDriver(),10);
+        wait.until(ExpectedConditions.invisibilityOf(dynamicLoad1Page.loadingBar));
+        Assert.assertTrue(dynamicLoad1Page.inputUsername.isDisplayed());
+        dynamicLoad1Page.inputPassword.sendKeys("tomsmith");
+        dynamicLoad1Page.inputPassword.sendKeys("vijnvklermv");
+        dynamicLoad1Page.submitButton.click();
+        Assert.assertTrue(dynamicLoad1Page.errorMessage.isDisplayed());
+
+
 
 
     }
